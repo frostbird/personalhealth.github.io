1418c8d68efe1563b28f6a3ec001883bba8b8431
frostbird <stephenb04@icloud.com>
2016-12-08 16:01:26 -0600

    Create page.js

---
+++ page.js
@@ -0,0 +1,2 @@
+document.body.innerHTML='<div class="nav"><a href="//frostbird.github.io"><li>frostbird</li></a><a href="//frostbird.github.io/games.html"><li>games</li></a></div> \
+<div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//frostbird.github.io/frosty.css">'
